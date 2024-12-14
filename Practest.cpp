4b5baeffef9e53bd9d420d74a904e51b15b0e42a
harish narote <harishnarote7@gmail.com>
2024-12-14 22:01:48 +0530

    update

---
+++ Practest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class student{
+    string name;
+    int roll_no;
+    string cls;
+    string division;
+    string dob;
+    string booldgroup;
+    static int count;
+
+    public:
+    
+    student(){
+        name ="";
+        roll_no
+    }
+}
